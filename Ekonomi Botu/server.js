f706a0e144254fce526f393ff1da6b8b28c509ef
merlinsalvo <88404623+merlinsalvo@users.noreply.github.com>
2021-08-03 23:31:20 +0300

    nou
    wqe

---
+++ Ekonomi Botu/server.js
@@ -0,0 +1,145 @@
+const express = require('express');
+const app = express();
+app.use(express.static("public"));
+const queue = new Map();
+const { apikey } = require('./ayarlar.json');
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const ayarlar = require('./ayarlar.json')
+const YouTube = require('simple-youtube-api');
+const ffmpeg = require('ffmpeg');
+const youtube = new YouTube('apikey');
+const ytdl = require('ytdl-core');
+const prefix = ayarlar.prefix;
+const fs = require('fs');
+const moment = require('moment');
+const Jimp = require('jimp');
+const chalk = require('chalk');
+app.get("/", function(request, response) {
+  response.sendFile(__dirname + "/views/index.html");
+});
+
+const listener = app.listen(process.env.PORT, function() {
+  console.log("Your app is listening on port " + listener.address().port);
+});
+
+const http = require('http');
+    app.get("/", (request, response) => {
+    console.log(`[PING] Açık tutuyorum...`);
+    response.sendStatus(200);
+    });
+    
+    setInterval(() => {
+    http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+    }, 280000);
+
+
+
+
+
+const db = require("quick.db")
+
+require('./util/eventLoader')(client);
+
+//////////////////////////////////////////////
+client.ayarlar = {
+"durum":"dnd",//online , idle , dnd 
+"oynuyor":"sa",
+"prefix":"w!",
+"sahip":"300573341591535617",
+"token":"Token"
+}
+/////////////////////////////////////////////
+client.ekoayarlar = {
+  parabirimi: "TL",
+  botunuzunprefixi: "w!",
+  botunuzunidsi: "637943210261479424",
+  botismi: "Woxe Deneme",
+  renk: "RANDOM", //İNGİLİZCE TERCİH ETTİĞİNİZ RENGİ YAZINIZ! EĞER BÖYLE BIRAKIRSANIZ RASTGELE ATAR!
+  isimsiz: "Bilinmiyor", //İSİMSİZ KİŞİLERİN HANGİ İSİM İLE GÖZÜKECEĞİNİ BELİRLEYEBİLİRSİNİZ!
+  rastgelepara: true, //EĞER BUNU TRUE YAPARSANIZ RASTGELE PARA VERME ÖZELLİĞİ AKTİF OLUR VE GÜNLÜK PARALARI RASTGELE VERİR!
+  minpara: 10, //EĞER RASTGELE PARA DURUMUNU AKTİF ETTİYSENİZ BURADAN RASTGELE PARA PARAMETRESİNİNİN MİNUMUM PARASINI BELİRTİNİZ!
+  maxpara: 200, //EĞER RASTGELE PARA DURUMUNU AKTİF ETTİYSENİZ BURADAN RASTGELE PARA PARAMETRESİNİNİN MAXİMUM PARASINI BELİRTİNİZ!
+  günlükpara: 50, //EĞER RASTGELE PARAYI TRUE YAPTIYSANIZ BURAYI ELLEMENİZE GEREK YOK!
+  dbloy: false, //EĞER BOTUNUZ DBL (DİSCORD BOT LİST) DE KAYITLIYSA GÜNLÜK ÖDÜL ALMAK İÇİN OY İSTER FALSE KAPALI, TRUE AKTİF DEMEK!
+  dblkey: "KEY", //EĞER DBLOY U AKTİF ETMEDİYSENİZ BURAYA KEY EKLEMENİZE GEREK YOK EĞER AKTİF ETTİYSENİZ DBL SİTESİNDEN BULABİLİRSİNİZ!
+  dblmsj: "Bu komutu kullanabilmek için bota oy vermelisiniz. Oy vermek için !oyver", //EĞER DBLOY U AKTİF ETMEDİYSENİZ BURAYA MESAJ YAZMANIZA GEREK YOK! EĞER AKTİF ETTİYSENİZ BOTA OY VERMEK İÇİN HANGİ MESAJI YAZACAĞINI AYARLAYABİLİRSİNİZ.
+  başlangıçparası: 50, //EĞER RASTGELE PARAYI TRUE YAPTIYSANIZ BURAYI ELLEMENİZE GEREK YOK!
+  admin: ["300573341591535617"]
+}
+const kurulum = message => {
+  console.log(`Kurulum: ${message}`);
+};
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir('./komutlar/', (err, files) => {
+  if (err) console.error(err);
+  kurulum(`${files.length} komut kurulacak.`);
+   kurulum(`-------------------------`);
+   files.forEach(f => {
+    let pingKodları = require(`./komutlar/${f}`);
+  
+    kurulum(`Kurulan komut ~ ${pingKodları.help.name}.`);
+    client.commands.set(pingKodları.help.name, pingKodları); 
+    kurulum(`-------------------------`);
+    client.commands.set(pingKodları.help.name, pingKodları);
+    pingKodları.conf.aliases.forEach(alias => {
+    client.aliases.set(alias, pingKodları.help.name);
+    });
+  });
+});
+
+client.reload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let pingDosya = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, pingDosya);
+      pingDosya.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, pingDosya.help.name);
+      });
+      resolve();
+    } catch (e){
+      reject(e);
+    }
+  });
+};
+
+client.load = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e){
+      reject(e);
+    }
+  });
+};
+
+client.unload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      resolve();
+    } catch (e){
+      reject(e);
+    }
+  });
+};
+
+
+client.login(ayarlar.token); 
